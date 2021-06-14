11583a1564b129a91bc0aa8fd87fde0730602e18
roger tu <zongyi0520@gmail.com>
2021-06-14 20:00:39 +0800

    class的練習, for database

---
+++ db.py
@@ -0,0 +1,9 @@
+class Db:
+    def __init__(self):
+        self.connect()
+    
+    def connect(self):
+        print('正在進行連線..........')
+    
+    def insert_data(self):
+        print('saljfljdlkjdljsa')
